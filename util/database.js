f364685e877147822916ad0122c415799330a7d8
sdoshi983 <sdoshi983@rku.ac.in>
2023-08-27 12:25:16 +0530

    feat(db): create database connect client

--- util/database.js
+++ util/database.js
@@ -1,5 +1,13 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete', 'root', 'password', { dialect: 'mysql', host: 'localhost' });
+const mongoConnect = (callback) => {
+    MongoClient.connect('mongodb+srv://sd:sdoshi983@cluster0.0nb30.mongodb.net/?retryWrites=true&w=majority')
+        .then(client => {
+            console.log('Connected!');
+            callback(client);
+        })
+        .catch(err => console.log(err));
+}
 
-module.exports = sequelize;
+module.exports = mongoConnect;
