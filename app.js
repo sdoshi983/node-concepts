f1cdf836fa2f7019113986d387c50eb55e63e4c0
sdoshi983 <sdoshi983@rku.ac.in>
2023-08-27 19:04:24 +0530

    feat(users): create a user model, and a dummy user in the every requests

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const errorController = require('./controllers/error');
 const mongoConnect = require('./util/database').mongoConnect;
+const User = require('./models/user');
 
 app.use(bodyParser.urlencoded({ extended: false }));      // to parse the data in the incoming request
 app.use(express.static(path.join(__dirname, 'public')));    // make the public folder available statically anywhere in the code, for read-only. Other folders are not accessible
@@ -22,13 +23,13 @@ join method concats the arguments in a single string.
 
 // Below we are registering a middleware for all the incoming request. Note that it is called at the top (before all the middlewares) so we will be having the user data before any incoming requests gets hit/fulfilled
 app.use((req, res, next) => {
-    // User.findByPk(1)
-    //     .then(user => {
-    //         // we are storing user object in the request object. As, by default req object doesn't have any such key as user. Hence it is like we are creating a new key and assign the user obejct to that key. 
-    //         req.user = user;     // NOTE: the user object we are assigning is not the javascript object, it is the sequelize object. Hence it will also have all the sequlize methods like destroy and all
-    //         next();
-    //     })
-    //     .catch(err => console.log(err));
+    User.findById("64eb501e817bbe9744caccec")
+        .then(user => {
+            // we are storing user object in the request object. As, by default req object doesn't have any such key as user. Hence it is like we are creating a new key and assign the user obejct to that key. 
+            req.user = user;
+            next();
+        })
+        .catch(err => console.log(err));
     next();
 })
 app.use('/admin', adminRoutes);     // filtering the admin routes. If a request has /admin in the beginnning, then only it will further go to the admin routes
