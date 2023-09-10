bb8ed72fbe1ea206c7a612b0219865f56a491631
sdoshi983 <sdoshi983@rku.ac.in>
2023-09-10 12:40:52 +0530

    feat(products): add the documentation and example of select and populate method of mongoose

--- controllers/admin.js
+++ controllers/admin.js
@@ -70,15 +70,19 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-    Product.find().then(products => {
-        res.render('admin/products', {
-            prods: products,
-            pageTitle: 'Admin Products',
-            path: '/admin/products',
-        })
-    }).catch(err => {
-        console.log(err);
-    });
+    Product.find()
+        // .select('title price -_id')     // this will return the products with only title and price key. And we have explicitly excluded _id
+        // .populate('userId')     // this will populate the whole user details, instead of just userId. This works becuase in the product model, we have set the ref of the userId key to the User model. Hence, when this method will get called, it will search this id through the users collection and populate the data
+        .then(products => {
+            console.log(products);
+            res.render('admin/products', {
+                prods: products,
+                pageTitle: 'Admin Products',
+                path: '/admin/products',
+            })
+        }).catch(err => {
+            console.log(err);
+        });
 };
 
 exports.postDeleteProduct = (req, res, next) => {
