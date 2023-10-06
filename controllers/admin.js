715f38659babc1df6fbb504e6b1474e12cedfb03
sdoshi983 <sdoshi983@rku.ac.in>
2023-10-06 20:17:03 +0530

    feat(auth): add protection on post routes like edit, delete etc, if the another user who hasn't created the product, tries to perform any actions

--- controllers/admin.js
+++ controllers/admin.js
@@ -63,22 +63,27 @@ exports.postEditProduct = (req, res, next) => {
 
     Product.findById(prodId)
         .then(product => {
+            if (product.userId !== req.user._id) {
+                return res.redirect('/');
+            }
+
             product.title = updatedTitle;
             product.price = updatedPrice;
             product.description = updatedDescription;
             product.imageUrl = updatedImageUrl;
-            return product.save();
+            return product.save()
+                .then(result => {
+                    console.log('UPDATED PRODUCT!');
+                    res.redirect('/admin/products');
+                });
         })
-        .then(result => {
-            console.log('UPDATED PRODUCT!');
-            res.redirect('/admin/products');
-        }).catch(err => {
+        .catch(err => {
             console.log(err);
         });
 };
 
 exports.getProducts = (req, res, next) => {
-    Product.find({userId: req.user._id})
+    Product.find({ userId: req.user._id })
         // .select('title price -_id')     // this will return the products with only title and price key. And we have explicitly excluded _id
         // .populate('userId')     // this will populate the whole user details, instead of just userId. This works becuase in the product model, we have set the ref of the userId key to the User model. Hence, when this method will get called, it will search this id through the users collection and populate the data
         .then(products => {
@@ -95,7 +100,7 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
     const prodId = req.body.productId;
-    Product.findByIdAndRemove(prodId)
+    Product.deleteONe({ _id: prodId, userId: req.user._id })
         .then(product => {
             console.log('DELETED PRODUCT!');
             res.redirect('/admin/products');
