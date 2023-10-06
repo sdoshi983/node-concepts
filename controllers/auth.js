b81235e5eaf79e79a11997215a6839b931846024
sdoshi983 <sdoshi983@rku.ac.in>
2023-10-06 21:01:29 +0530

    feat(validation): use express validator package on email field while sign up

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const bcrypt = require('bcryptjs');
 const nodemailer = require('nodemailer');
 const sendgridTransport = require('nodemailer-sendgrid-transport');
 const crypto = require('crypto');
+const { validationResult } = require('express-validator/check');
 
 const User = require('../models/user');
 
@@ -78,6 +79,15 @@ exports.postSignup = (req, res, next) => {
     const email = req.body.email;
     const password = req.body.password;
     const confirmPassword = req.body.confirmPassword;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).render('auth/signup', {
+            path: '/signup',
+            pageTitle: 'Signup',
+            isAuthenticated: false,
+            errorMessage: errors.array(),
+        })
+    }
 
     User.findOne({ email: email })
         .then(userDoc => {
